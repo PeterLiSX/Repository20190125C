d6951ed2ee7ab1cdcce6e9a7d2a22d77d972c22f
Peter.X.Li <peter.x.li@newegg.com>
2019-01-25 16:10:09 +0800

    Add file2

---
+++ Data/Action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.PaymentService.Data
+{
+    public enum Action
+    {
+        SaveBathOrderPaymentRequest,
+        TriggerBatchCaptureRequest,
+        GetBatchOrderFPStatus,
+        GetBatchOrderPaymentResultInfo,
+        ChargeCustomer,
+        UpdateSOCreditCard,
+        SaveManualCharge,
+        SaveAuthRequest
+    }
+}
